8c063b8a0dbe34c2546660c6e10e7d4153581d14
Ryan Ginsburg <121274374+RyanGinsburg@users.noreply.github.com>
2023-06-13 16:59:09 -0400

    Update app.py

--- Digital Minds/project/app.py
+++ Digital Minds/project/app.py
@@ -1,16 +1,14 @@
 import os
-
-from functools import wraps
-
-from flask import Flask, flash, redirect, render_template, url_for, request, session
-from flask_socketio import join_room, leave_room, send, SocketIO
-from flask_session import Session
-import random
-from string import ascii_uppercase
-from tempfile import mkdtemp
-from werkzeug.security import check_password_hash, generate_password_hash
 import datetime
-from cs50 import SQL
+import functools
+import cs50
+import flask
+import flask_socketio
+import flask_session
+import string
+import tempfile
+import werkzeug.security
+
 
 
 # API KEY: export API_KEY=pk_06bfd2c6157948b2a9751c1e2bcc1f69
@@ -692,6 +690,5 @@ def disconnect():
         app.run(host="0.0.0.0",  port=5000)
         socketio.run(app, debug=True)
         redirect("/")
-
 
 
